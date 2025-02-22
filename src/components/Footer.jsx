dfc470260d43ede75d44429605aee6ad3f0ba2fe
Watty_Sylvester <88721196+masindes@users.noreply.github.com>
2025-02-23 01:42:53 +0300

    footer adjusted codes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="bg-white text-black py-6 mt-10 border-t border-gray-300">
+    <footer className="bg-white text-black border-t border-gray-300 pb-10 shadow-lg pt-6">
       <div className="max-w-6xl mx-auto flex flex-col md:flex-row justify-between items-center">
         
         {/* Logo Section */}
         <div className="flex items-center space-x-3">
           <img 
-            src="src/assets/images/moringa-01.png" 
+            src="src/assets/images/moringa-01.png"  // Ensure this path is correct
             alt="Moringa Logo" 
             className="w-24 md:w-28" 
           />
